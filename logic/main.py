67cb20a1cafde47acbe95b640e3528479262fac5
Tan Kim Hao <tankimhao2@gmail.com>
2025-07-16 23:10:25 +0800

    make the merchant to list

--- logic/main.py
+++ logic/main.py
@@ -1,16 +1,19 @@
 import streamlit as st
 from esg_logic import get_merchant_category, get_esg_tag, get_donation_message
+from esg_logic import merchant_map 
 
-st.title("🌱 ESG Tagging & Donation (Demo)")
+st.title("🌱  ESG Tagging & Donation")
 
-merchant = st.text_input("Enter Merchant Name (e.g., Grab, Shopee, TNB)").strip()
+
+merchant = st.selectbox("Select a Merchant", list(merchant_map.keys()))
+
 
 if merchant:
     category = get_merchant_category(merchant)
     esg_tag = get_esg_tag(category)
 
-    st.markdown(f"** Merchant Category:** `{category}`")
-    st.markdown(f"** ESG Tag:** `{esg_tag}`")
+    st.markdown(f"**🧾 Merchant Category:** `{category}`")
+    st.markdown(f"**📊 ESG Tag:** `{esg_tag}`")
 
     if esg_tag in ["High Impact", "Medium Impact"]:
         donate = st.checkbox("Donate RM0.50 to Red Cross to offset this impact")
@@ -18,10 +21,10 @@ if merchant:
         if msg:
             st.success(msg)
     elif esg_tag == "Positive Impact":
-        st.success("Great! This merchant supports sustainability.")
-    elif esg_tag == "Low Impact":
-        st.info("This merchant has minimal ESG impact.")
+        st.success("🌟 Great! This merchant supports sustainability.")
+    elif esg_tag == "✅ Low Impact":
+        st.info("ℹ️ This merchant has minimal ESG impact.")
     elif esg_tag == "Neutral":
         st.info("stThis merchant is neutral in ESG terms.")
     else:
-        st.warning("ESG info not available for this merchant.")
+        st.warning("⚠️ ESG info not available for this merchant.")
